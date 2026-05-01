c5445c4a99bd501ff9cbb757fbfa801eb9508df4
hansol0118 <wall197944@naver.com>
2026-05-01 12:32:37 +0900

    5,6,7,8

---
+++ backend/src/main/java/com/back/RestApiApplication.kt
@@ -0,0 +1,14 @@
+package com.back
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+open class RestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestApiApplication>(*args)
+}
+
